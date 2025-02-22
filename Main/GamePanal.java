dfb76412409b82154f0d09f1522e73a2d4b51901
WAMAK <yuttaphonp23@gmail.com>
2025-02-22 22:46:04 +0700

    gameThreadStart i think work

--- Main/GamePanal.java
+++ Main/GamePanal.java
@@ -6,39 +6,81 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+
+
+
+
+
+
 public class GamePanal extends JPanel implements Runnable{
     final int screenWidth = 1280;
     final int screenHeight = 720;
     ImageIcon background ; 
- 
+    Thread gameThread;
+    // flexible 
+    int FPS = 60;
+    
 
     public GamePanal(){
-        
+
         this.loadedblackground();
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setFocusable(true);
-        
+     
     }
 
-     public void paintComponent(Graphics g){
-            super.paintComponent(g);
-            g.drawImage(background.getImage(), 0, 0, screenWidth, screenHeight, null);
+    public void StartGameThread(){ 
+       
+        gameThread = new Thread(this);
+        gameThread.start(); // tell run 
+
     }
+
+
+     
 
 
     public void loadedblackground(){
+
         background = new ImageIcon(this.getClass().getResource("/res/Background/back_01.jpg"));
     }
 
-
+ @Override
     public void run(){
+        double drawInterval = 1000000000 / (FPS); // วาดทุก 0.016 second
+        double delta  = 0 ;
+        long lasttime  =System.nanoTime();
+        long currentTime;
+        long timer = 0 ;
+        int drawCount = 0 ;
 
+        while(gameThread != null){
+            currentTime = System.nanoTime() ;
+            delta += (currentTime - lasttime) / drawInterval;
+            timer += (currentTime - lasttime);
+            lasttime = currentTime ; 
 
+            if(delta >= 1 ){
+                update();
         
+                repaint();
+                delta -- ; 
+                drawCount ++;
+            }
 
-
-        
+            if(timer >= 1000000000){
+                System.out.println("FPS "+drawCount);
+                drawCount = 0 ;
+                timer = 0;
+            }
+        }
     }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(background.getImage(), 0, 0, screenWidth, screenHeight, null);
+}
+
 
     public void update() {
 
--- Main/Main.java
+++ Main/Main.java
@@ -17,5 +17,6 @@ public class Main {
         window.pack();
         window.setLocationRelativeTo(null); // กลางจอ
         window.setVisible(true); // ให้เห็น
+        gamepanal.StartGameThread();
     }
 }
