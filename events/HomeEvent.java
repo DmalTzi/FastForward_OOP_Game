03609455233af58b0e5dffacdef81c8faf3d57a8
Tests <Audyomg@gmail.com>
2025-03-03 23:45:00 +0700

    test

--- events/HomeEvent.java
+++ events/HomeEvent.java
@@ -1,6 +1,9 @@
 package events;
 
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JLabel;
 
 import main.GamePanel;
@@ -15,5 +18,16 @@ public class HomeEvent extends SuperEvents {
         super.btn.setContentAreaFilled(false);
         super.btn.setBorder(null);
         super.setBound(500, 100, btn.getIcon().getIconWidth(), btn.getIcon().getIconHeight());
+        super.btn.addMouseListener(new MouseListener() {
+                public void mouseEntered(MouseEvent e) {}
+                public void mouseReleased(MouseEvent e) {
+                    System.out.println("Home Event");
+                    setMenuVisible(true);
+                    gp.setShowEvent(false);
+                }
+                public void mouseExited(MouseEvent e) {}
+                public void mouseClicked(MouseEvent e) {}
+                public void mousePressed(MouseEvent e) {}
+        });
     }
 }
